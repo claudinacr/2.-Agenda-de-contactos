b2b4ccfcedf15c297060e86137f9392a623b1596
Claudina Cortez <claudinacr@gmail.com>
2017-12-12 20:35:13 -0300

    Implementacion boton borrar registro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,18 @@
-$(document).ready(function () {
-  $('#agregar').click(function () {
-    //creacion de una variable comentario que es igual a
-    //id del textarea
-    var nombre = $('#nombre').val();
-    var telefono = $('#telefono').val();
+$(document).ready(function() {
+    $('#agregar').click(function() {
+        //creacion de una variable comentario que es igual a
+        //id del textarea
+        var nombre = $('#nombre').val();
+        var telefono = $('#telefono').val();
 
-    $('#nombre').val("");
-    $('#telefono').val("");
-    //#cont es el id del contenedor que contendrá el comentario
-    var contenedor = $('.boxContainer');
-    contenedor.append('<div>' + '<h3>' + nombre + '</h3>' + '<p>' + telefono + '</p>' + '<i class= "fa fa-trash trash">' + '</i>');
-  });
-  $('.fa-trash').click(function () {
+        $('#nombre').val("");
+        $('#telefono').val("");
+        //#cont es el id del contenedor que contendrá el comentario
+        var contenedor = $('.boxContainer');
+        contenedor.append('<div>' + '<h3>' + nombre + '</h3>' + '<p>' + telefono + '</p>' + '<button type="button" class="btn trash" aria-expanded="false"><i class= "fa fa-trash"></i></button>');
+    });
 
-  });
+    $('.boxContainer').on('click', '.trash', function() {
+        $(this).parent().remove();
+    });
 });
